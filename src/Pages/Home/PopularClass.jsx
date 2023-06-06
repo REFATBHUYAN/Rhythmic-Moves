3502ee5eef2a4a8a87e5241631bf4e2ae33eb233
REFATBHUYAN <refatbubt@gmail.com>
2023-06-07 01:53:45 +0600

    popular classes section added

--- src/Pages/Home/PopularClass.jsx
+++ src/Pages/Home/PopularClass.jsx
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const PopularClass = () => {
   const card = [1, 2, 4, 5, 5, 6, 7, 4];
+  const [classes, setClasses] = useState([]);
+  useEffect(()=>{
+    fetch('/dance.json')
+    .then(res => res.json())
+    .then(data =>{
+        console.log(data);
+        setClasses(data.slice(0,6));
+    })
+  },[])
   return (
-    <div>
-      <h1 className="text-5xl font-bold text-center">Popular Classes</h1>
+    <div className="py-10">
+      <h1 className="text-5xl font-bold text-center py-8">Popular Classes</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {card.map((item, i) => (
+        {classes.map((item, i) => (
           <div key={i} className="card card-compact w-96 bg-base-100 shadow-xl">
             <figure>
               <img
-                src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                src={item.image}
                 alt="Shoes"
+                className="h-60 rounded-md"
               />
             </figure>
             <div className="card-body">
-              <h2 className="card-title">Shoes!</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <h2 className="card-title">{item.className}</h2>
+              <p>Instructor: {item.instructorName}</p>
+              <p>Available Seats: {item.availableSeats}</p>
+              <p>Price: {item.price}</p>
               <div className="card-actions justify-end">
                 <button className="btn btn-primary">Buy Now</button>
               </div>
